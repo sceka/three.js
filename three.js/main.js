aad25c24c96841beff226cd62b2ccd038522fade
sceka <scekicm02@gmail.com>
2022-03-13 16:15:01 +0100

    Add scene, camera and renderer

--- three.js/main.js
+++ three.js/main.js
@@ -4,3 +4,14 @@ import * as THREE from "https://cdn.skypack.dev/three@0.126.1";
 
 const scene = new THREE.Scene();
 console.log(scene);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  innerWidth / innerHeight,
+  0.1,
+  1000
+);
+console.log(camera);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(innerWidth, innerHeight);
+document.body.appendChild(renderer.domElement);
+console.log(renderer);
