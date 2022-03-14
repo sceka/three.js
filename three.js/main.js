95f0a6cb7d3d8230f2f129bd3e4f437dcf8d28fe
sceka <scekicm02@gmail.com>
2022-03-14 04:19:13 +0100

    Add dat.gui and interact with it

--- three.js/main.js
+++ three.js/main.js
@@ -1,17 +1,67 @@
 // Find the latest version by visiting https://cdn.skypack.dev/three.
 
 import * as THREE from "https://cdn.skypack.dev/three@0.126.1";
+import * as dat from "dat.gui";
+const gui = new dat.GUI();
+const world = {
+  plane: {
+    width: 10,
+    height: 10,
+    widthSegments: 10,
+    heightSegments: 10,
+  },
+};
+gui.add(world.plane, "width", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "height", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "widthSegments", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "heightSegments", 1, 20).onChange(generatePlane);
+
+function generatePlane() {
+  planeMesh.geometry.dispose();
+  planeMesh.geometry = new THREE.PlaneGeometry(
+    world.plane.width,
+    world.plane.height,
+    world.plane.widthSegments,
+    world.plane.heightSegments
+  );
+  const { array } = planeMesh.geometry.attributes.position;
+
+  for (let i = 0; i < array.length; i++) {
+    const x = array[i];
+    const y = array[i + 1];
+    const z = array[i + 2];
+
+    array[i + 2] = z + Math.random();
+  }
+}
 
 const scene = new THREE.Scene();
-console.log(scene);
 const camera = new THREE.PerspectiveCamera(
   75,
   innerWidth / innerHeight,
   0.1,
   1000
 );
-console.log(camera);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
 document.body.appendChild(renderer.domElement);
-console.log(renderer);
+camera.position.z = 5;
+
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+const planeMaterial = new THREE.MeshPhongMaterial({
+  color: 0xff0000,
+  side: THREE.DoubleSide,
+  flatShading: THREE.FlatShading,
+});
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(planeMesh);
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 0, 1);
+scene.add(light);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
